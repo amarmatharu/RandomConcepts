ebe995e3b9b7e9eb46920cc7e988fb5ad9219366
Robin James <rjames@bdnacorp.com>
2017-04-02 17:26:52 -0700

    modified DAO interface

--- JmeterTestUtility/src/main/java/com/bdna/automation/dao/Dao.java
+++ JmeterTestUtility/src/main/java/com/bdna/automation/dao/Dao.java
@@ -1,12 +1,6 @@
 package com.bdna.automation.dao;
 
-import java.util.List;
-
-import com.bdna.automation.entity.BdnaTechnopediaTag;
-
-public interface DIMDao {
-
-	public List<BdnaTechnopediaTag> getBDNATechnopediaTag();
+public interface Dao {
 
 	public int getCount(String query);
 
