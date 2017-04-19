b58919c1f8515ab2f5f6f062d9f2eae8c77f547c
Amardeep Singh <asingh@bdna.com>
2017-04-18 18:16:09 -0700

    Added Property reader

---
+++ JmeterTestUtility/src/main/java/com/bdna/automation/utility/PropertiesFileReader.java
@@ -0,0 +1,35 @@
+package com.bdna.automation.utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class PropertiesFileReader {
+
+	public void readValue() {
+
+		try {
+			File file = new File("jmeter.properties");
+			FileInputStream fileInput = new FileInputStream(file);
+			Properties properties = new Properties();
+			properties.load(fileInput);
+			fileInput.close();
+
+			Enumeration enuKeys = properties.keys();
+			while (enuKeys.hasMoreElements()) {
+				String key = (String) enuKeys.nextElement();
+				String value = properties.getProperty(key);
+				System.out.println(key + ": " + value);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
