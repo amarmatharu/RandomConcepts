61ba075981e3293b949d7de8e03c223a6d4d47af
amarmatharu <amardeep.matharu@gmail.com>
2018-02-10 20:23:50 -0800

    Add files via upload
    Two Sum Problem 1

---
+++ TwoSum_Problem1.java
@@ -0,0 +1,66 @@
+package com.amar.leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ * 
+ * 
+ */
+public class TwoSum_Problem1 {
+
+	public static int[] twoSum(int[] nums, int target) {
+
+		// to store the numbers which is less than target
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int remainder = 0;
+		int[] result = new int[2];
+
+		for (int i = 0; i < nums.length; i++) {
+			int start = nums[i];
+
+			// check if the number is less than target
+
+			// what is the remainder we are looking for
+			remainder = target - start;
+
+			if (map.containsKey(remainder)) {
+				// return the indexes; we have found two numbers
+				result[1] = i;
+				result[0] = map.get(remainder);
+
+			} else {
+				// not found in map then add it in the map
+				map.put(start, i);
+
+			}
+
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		int[] nums = { -3, 4, 3, 90 };
+		int target = 0;
+
+		TwoSum_Problem1 obj = new TwoSum_Problem1();
+		int[] result = twoSum(nums, target);
+
+		for (int i : result) {
+			System.out.println(i);
+		}
+
+	}
+
+}
